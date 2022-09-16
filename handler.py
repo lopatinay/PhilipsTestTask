baf95eefa96d25d31c5a0295e6e548ef453e4e04
Alexander Lopatin <lopatin.alexander.y@gmail.com>
2022-09-16 16:34:52 +0400

    init

---
+++ handler.py
@@ -0,0 +1,16 @@
+import json
+
+
+def hello(event, context):
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "input": event,
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
+
+
+if __name__ == "__main__":
+    print(hello(None, None))
